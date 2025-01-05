b0c2f641df025bccbe14332b1b548bbf81ed2c62
YatharthChauhan2362 <yc232326@gmail.com>
2025-01-06 02:18:42 +0530

    latest

--- src/components/Products/types.ts
+++
@@ -1,7 +0,0 @@
-export interface Product {
-  title: string;
-  description: string;
-  url: string;
-  icon: React.ReactNode;
-  gradient: string;
-}
--- src/types/youtube.ts
+++
@@ -1,7 +0,0 @@
-export interface YouTubeVideo {
-  id: string;
-  title: string;
-  thumbnail: string;
-  publishedAt: Date;
-  description: string;
-}
