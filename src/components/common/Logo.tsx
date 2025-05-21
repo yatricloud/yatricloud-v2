924819599640ebcca849825952e069704828a334
Yatharth Chauhan <yc232326@gmail.com>
2025-05-21 13:34:00 +0530

    latest

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -8,7 +8,7 @@ export function Logo({ className = '' }: LogoProps) {
   return (
     <div className={`flex items-center gap-3 ${className}`}>
       <img 
-        src="https://dev.yatricloud.com/assets/img/yatricloud.png" 
+        src="https://raw.githubusercontent.com/yatricloud/yatri-images/refs/heads/main/Logo/yatricloud-round-transparent.png" 
         alt="Yatri Cloud"
         className="h-8 w-auto"
       />
